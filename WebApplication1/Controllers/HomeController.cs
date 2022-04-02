cdd351a635dc4881d6f1a370575aad6dcd2a68d7
Yoshihiro123 <yoshihirotakaki735@gmail.com>
2022-04-02 13:10:13 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new UserModel());
+        }
+
+        /// <summary>
+        /// 名前をpostされた時の処理
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index([Bind(Include = "Name")] UserModel user)
+        {
+            return RedirectToAction("Hello", user);
+        }
+
+        public ActionResult Hello([Bind(Include = "Name")] UserModel user)
+        {
+            if (string.IsNullOrEmpty(user.Name))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace WebApplication1.Models
+{
+    public class UserModel
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        [DisplayName("お名前")]
+        public string Name { get; set; }
+    }
+}
